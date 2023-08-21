846bb57a0fb4d8ad0549538f5cfbf3637c32345c
Josimar Souza <contact.josimarsouza@gmail.com>
2023-08-21 15:31:06 -0300

    feat: Info added to game item card

--- src/components/GameItem/gameItemStyles.js
+++ src/components/GameItem/gameItemStyles.js
@@ -1,10 +1,36 @@
 import styled from 'styled-components/native';
 
-const GameContainer = styled.View`
-  border: 1px solid green;
-  height: 320px;
+export const GameContainer = styled.View`
+  background-color: #9AC5F4;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  height: 550px;
+  justify-content: space-around;
   margin: 10px auto;
-  width: 80%;
+  padding: 10px;
+  width: 85%;
 `;
 
-export default GameContainer;
+export const GameImage = styled.Image`
+  border-radius: 15px;
+  height: 70%;
+  margin: 0 auto;
+  width: 70%;
+`;
+
+export const InfoContainer = styled.View`
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  min-height: 50px;
+  width: 100%;
+`;
+
+export const Title = styled.Text`
+  text-align: center;
+  font-size: 20px;
+`;
+
+export const Info = styled.Text``;
--- src/components/GameItem/index.jsx
+++ src/components/GameItem/index.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import { format } from 'date-fns';
 
-import GameContainer from './gameItemStyles';
+import {
+  GameContainer,
+  GameImage,
+  InfoContainer,
+  Info,
+  Title,
+} from './gameItemStyles';
 
-const GameItem = (game) => {
+const GameItem = ({ game }) => {
+  const {
+    image,
+    title,
+    developer,
+    releaseDate,
+  } = game;
+
   console.log(game);
+
   return (
-    <GameContainer />
+    <GameContainer>
+      <GameImage
+        source={{uri: image}}
+        alt={`Imagem do jogo ${title}`}
+        resizeMode="stretch"
+      />
+      <Title>{title}</Title>
+      <InfoContainer>
+        <Info>{developer}</Info>
+        <Info>{format(new Date(releaseDate), 'dd/MM/yyyy')}</Info>
+      </InfoContainer>
+    </GameContainer>
   )
 };
 
